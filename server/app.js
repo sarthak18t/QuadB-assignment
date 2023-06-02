8c63afe4390f0db02035ee9dcc906f54e57d01a3
[sarthak18t] <[sarthaktailor@gmail.com]>
2023-06-02 22:19:53 +0530

    backend completed

--- server/app.js
+++ server/app.js
@@ -17,5 +17,7 @@ app.get('/',(req,res)=>{
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
+    
     db()
+
 })
--- server/database/conn.js
+++ server/database/conn.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose')
 const {fetchData} = require('../api_data/data')
-const Ticker = require('../model/model')
+const model = require('../model/model')
 const connect = async()=>{
-    await mongoose.connect(process.env.MONGO_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    })
-    .then(()=>{
-        console.log('database connected')
-    })
-    .then(()=>{
+
+    try {
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }).then(()=>{
+            console.log("Database connected")
+        })
         
-        fetchData()
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+        await model.deleteMany({})
+
+        await fetchData()
+    } catch (error) {
+        console.log("Error in fetching " ,error)
+    }
+
 } 
 
 module.exports = connect
